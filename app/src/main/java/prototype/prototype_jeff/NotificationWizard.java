985bc3a7876e92d3420b17354f623aa4414ccad8
Brian French <bri.r.french@gmail.com>
2017-11-02 18:57:31 -0400

    This should contain new gui. press begin button to get there

---
+++ app/src/main/java/prototype/prototype_jeff/NotificationWizard.java
@@ -0,0 +1,89 @@
+package prototype.prototype_jeff;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class NotificationWizard extends AppCompatActivity {
+
+
+    Spinner spinner;
+    EditText email;
+    EditText phoneNumber;
+
+    CheckBox phoneBox;
+    CheckBox emailBox;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_wizard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        //Init of Object
+        phoneNumber = (EditText) findViewById(R.id.phoneText);
+        email = (EditText) findViewById(R.id.emailText);
+
+        phoneBox = (CheckBox) findViewById(R.id.phoneBox);
+        emailBox = (CheckBox) findViewById(R.id.emailBox);
+
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dropdown_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        phoneBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(phoneBox.isChecked()){
+                    phoneNumber.setVisibility(View.VISIBLE);
+                }else{
+                    phoneNumber.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+        emailBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(emailBox.isChecked()){
+                    email.setVisibility(View.VISIBLE);
+                }else{
+                    email.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+}
