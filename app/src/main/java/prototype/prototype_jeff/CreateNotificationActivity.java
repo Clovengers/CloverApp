228118d4bca012313996e01473de74c211a97bb0
Jeff <jeffreylehman94@gmail.com>
2017-11-05 01:13:07 -0400

    ??

--- app/src/main/java/prototype/prototype_jeff/CreateNotificationActivity.java
+++ app/src/main/java/prototype/prototype_jeff/CreateNotificationActivity.java
@@ -19,6 +19,7 @@ public class CreateNotificationActivity extends AppCompatActivity {
 
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notification);
