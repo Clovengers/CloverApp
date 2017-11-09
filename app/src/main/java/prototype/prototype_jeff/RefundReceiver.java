6e54fdca0a277011a7c678ae763df9ec9168b358
Brian French <bri.r.french@gmail.com>
2017-11-09 12:01:59 -0500

    This should fix it

--- app/src/main/java/prototype/prototype_jeff/RefundReceiver.java
+++ app/src/main/java/prototype/prototype_jeff/RefundReceiver.java
@@ -1,8 +1,11 @@
 package prototype.prototype_jeff;
 
+import android.accounts.Account;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AlertDialog;
@@ -10,13 +13,10 @@ import android.util.Log;
 
 import com.clover.sdk.v1.BindingException;
 import com.clover.sdk.v1.ClientException;
-import com.clover.sdk.v1.ServiceException;
-import com.clover.sdk.v3.order.OrderConnector;
-
-
 import com.clover.sdk.v1.Intents;
-import android.accounts.Account;
+import com.clover.sdk.v1.ServiceException;
 import com.clover.sdk.v3.order.Order;
+import com.clover.sdk.v3.order.OrderConnector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +37,7 @@ public class RefundReceiver extends BroadcastReceiver {
     PopupActivity pa = new PopupActivity();
 
     @Override
+
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (action.equals(Intents.ACTION_ORDER_CREATED) || action.equals(Intents.ACTION_REFUND)) {
@@ -52,10 +53,12 @@ public class RefundReceiver extends BroadcastReceiver {
                 Log.d("DEBUGGER_JEFF", "ORDER CONNECTOR CONNECTED ");
             }
             //ERROR HAPPENS IN TRY BLOCK, SERVICE INVOKED ON MAIN THREAD
-            try{lastOrder=orderConnector.getOrder(lastOrderId);
-                Log.d("LAST ORDER: ", lastOrder.toString());
-            }
-            catch (RemoteException e) {
+
+                //lastOrder=orderConnector.getOrder(lastOrderId);
+                //Log.d("DEBUGGERTESTING", lastOrder.toString());
+                new OrderAsyncTask().execute();
+
+           /* catch (RemoteException e) {
                 e.printStackTrace();
             } catch (ClientException e) {
                 e.printStackTrace();
@@ -63,7 +66,7 @@ public class RefundReceiver extends BroadcastReceiver {
                 e.printStackTrace();
             } catch (BindingException e) {
                 e.printStackTrace();
-            }
+            }*/
 
 
 
@@ -93,4 +96,55 @@ public class RefundReceiver extends BroadcastReceiver {
         }
 
     }
+
+
+
+    private class OrderAsyncTask extends AsyncTask<Void, Void, Order> {
+
+        @Override
+        protected final Order doInBackground(Void... params) {
+            String orderId = null;
+            Cursor cursor = null;
+            try {
+
+                lastOrder=orderConnector.getOrder(lastOrderId);
+                Log.d("LAST ORDER: ", lastOrder.toString());
+
+
+
+                if (orderId == null) {
+                    orderConnector.disconnect();
+                } else {
+                    return lastOrder;
+                }
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (ClientException e) {
+                e.printStackTrace();
+            } catch (ServiceException e) {
+                e.printStackTrace();
+            } catch (BindingException e) {
+                e.printStackTrace();
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected final void onPostExecute(Order order) {
+
+        }
+    }
+
+
+
+
+
+
+
+
 }
