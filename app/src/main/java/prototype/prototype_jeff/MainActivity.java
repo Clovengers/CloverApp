b48cac01c0bb20e78ab13cae39e98adb8577207f
TheHairyOne <montaguet3@students.rowan.edu>
2017-12-11 22:18:55 -0500

    Variable dismiss not being found

--- app/src/main/java/prototype/prototype_jeff/MainActivity.java
+++ app/src/main/java/prototype/prototype_jeff/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.clover.sdk.util.CloverAccount;
 import com.clover.sdk.v3.order.OrderConnector;
@@ -153,31 +154,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogStyle).setTitle("Popup Functionality").setMessage("Enter your email");
+                builder = new AlertDialog.Builder(MainActivity.this);
+                final AlertDialog alert  = builder.create();
+                View mView = getLayoutInflater().inflate(R.layout.pop_up_activity, null);
+                final EditText mEmail = (EditText) mView.findViewById(R.id.etEmail);
+                Button mCancel = (Button) mView.findViewById(R.id.dismiss);
+                Button mSubmit2 = (Button) mView.findViewById(R.id.etSubmit);
+                mSubmit2.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        if(!mEmail.getText().toString().isEmpty()){
+                            Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Login Failed", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
                 // Set up the input
-                final EditText input = new EditText(MainActivity.this);
-                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-                builder.setView(input);
+                //final EditText input = new EditText(MainActivity.this);
+                //input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                //builder.setView(input);
 
                 // Set up the buttons
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        inputText = input.getText().toString();
-                    }
+                //builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    //@Override
+                  //  public void onClick(DialogInterface dialog, int which) {
+                  //      inputText = input.getText().toString();
+           //           }
                 });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                mCancel.setOnClickListener(new View.OnClickListener(){
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
+                    public void onClick(View view){
+                        alert.dismiss();
                     }
-                });
+                                           });
+               // builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                 //   @Override
+                   // public void onClick(DialogInterface dialog, int which) {
+                    //    dialog.cancel();
+                    //}
+                //});
+
+                builder.setView(mView);
+                AlertDialog dialog = builder.create();
+                dialog.show();
 
 
-                builder.show();
-
-
-            }
+                }
         });
 
         settingsButton.setOnClickListener(new View.OnClickListener() {
